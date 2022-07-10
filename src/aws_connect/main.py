b39e7f4564df716a9331d2c808e6ba9172e90ac0
ac75019 <82723821+ac75019@users.noreply.github.com>
2022-07-10 21:07:50 +0100

    testing

--- src/aws_connect/main.py
+++ src/aws_connect/main.py
@@ -1,18 +1,18 @@
 # This is a sample Python script.
 import boto3
 
-client = boto3.client('emr')
+#client = boto3.client('emr')
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(dir(boto3)) # Press Ctrl+F8 to toggle the breakpoint.
+    return 0
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(dir(client))
+    main()
 
 
--- tests/test_0.py
+++ tests/test_0.py
@@ -1,9 +1,10 @@
 import unittest
+from aws_connect.main import main
 
 
 class ThisTest(unittest.TestCase):
     def test_code(self):
-        pass
+        main()
 
 
 if __name__ == '__main__':
